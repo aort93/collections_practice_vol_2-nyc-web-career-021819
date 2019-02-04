5d2ec6cc23b8fdf35049225cb2b158bc8b1134c9
Alonso Ortega <alonsojortega@gmail.com>
2019-02-04 08:17:37 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,46 @@
-# your code goes here
+def begins_with_r(array)
+  array.each do |element|
+    if element[0] != "r"
+      return false
+    end
+  end
+
+  return true
+end
+
+def contain_a(array)
+  newArr = []
+
+  array.each do |element|
+    if element.include?("a")
+      newArr.push(element)
+    end
+  end
+
+  newArr
+end
+
+def first_wa(array)
+  array.each do |element|
+    if element[0] == "w" && element[1] == 'a'
+      return element
+    end
+  end
+end
+
+
+def remove_non_strings(array)
+  newArr = []
+
+  array.each do |element|
+    if element.instance_of? String
+      newArr.push(element)
+    end
+  end
+
+  newArr
+end
+
+def count_elements(array)
+  
+end
